879f5fc553690866b3c3e6211c92581a8e96cf89
sakaury <lpague111@gmail.com>
2024-08-25 22:12:23 +0700

    sss

--- My-website/src/components/Homepage.jsx
+++ My-website/src/components/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import PostForm from './Post'
 function Homepage() {
   return <>
   <Navbar/>
-  <h1>This is home page</h1> 
+  <PostForm/>
   <Footer/>
  
   </>
