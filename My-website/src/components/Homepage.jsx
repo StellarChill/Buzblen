951dd053171d55d5e1b042041096b87bbf4d1d9d
MariyaWannarat <mxrrypx@gmail.com>
2024-09-13 20:21:26 +0700

    footer homepage

--- My-website/src/components/Homepage.jsx
+++ My-website/src/components/Homepage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Footer from './Footer'
 import PostForm from './Post'
+
 function Homepage() {
   return <>
   <Navbar/>
   <PostForm/>
+<<<<<<< HEAD
   <Footer/>
  
+=======
+>>>>>>> 38a290c9b7ac962eb894e720c4328f07135a8474
   </>
 }
 
