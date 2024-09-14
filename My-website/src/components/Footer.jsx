ef120636d7f03cf5e435a429d25f2ac17cf06e09
sakaury <lpague111@gmail.com>
2024-09-14 09:45:02 +0700

    chagne color footer

--- My-website/src/components/Footer.jsx
+++ My-website/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import  "../cssfile/Footer.css"
 function Footer() {
   return (
-    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+    <footer className="footer footer-center  text-base-content p-4">
     <aside>
       <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
     </aside>
