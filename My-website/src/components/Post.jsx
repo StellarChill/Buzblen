d3d26d7067bf74b8395421e05df70ce903441ee1
sakaury <lpague111@gmail.com>
2024-09-16 19:50:47 +0700

    add icon

--- My-website/src/components/Post.jsx
+++ My-website/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function PostForm() {
