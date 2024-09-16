30bd23c2c620d4f7afb35c9e74781fe53ae7687d
sakaury <lpague111@gmail.com>
2024-09-16 11:47:02 +0700

    add

--- My-website/src/components/Homepage.jsx
+++ My-website/src/components/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import PostForm from './Post'
+import Footer from './Footer'
 
 function Homepage() {
   return <>
   <Navbar/>
   <br />
   <PostForm/>
-  <Footer/>
+ 
  
   <br />
   <br />
