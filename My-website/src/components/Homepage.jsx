93a0cc5e060e368f5ecc6d59b06e9f3bf90bf1be
sakaury <lpague111@gmail.com>
2024-09-16 10:21:50 +0700

    change footer and homepage

--- My-website/src/components/Homepage.jsx
+++ My-website/src/components/Homepage.jsx
@@ -7,13 +7,8 @@ function Homepage() {
   <Navbar/>
   <br />
   <PostForm/>
-<<<<<<< HEAD
-<<<<<<< HEAD
   <Footer/>
  
-=======
->>>>>>> 38a290c9b7ac962eb894e720c4328f07135a8474
-=======
   <br />
   <br />
   <br />
@@ -27,7 +22,6 @@ function Homepage() {
   <br />
   <br />
   <Footer/>
->>>>>>> cf276d4782ce864304d06118783d89fe83932b1f
   </>
 }
 
